e48519b0002d6304b584cc5ef39cca07e0bf694f
mgn-migina <migina.2525@gmail.com>
2026-06-16 22:57:15 +0900

    バッチファイルによるサイト自動更新

---
+++ js/general.js
@@ -0,0 +1,87 @@
+var window_size     = $(window).width(),
+    header_point;
+const menuBtn       = $('.manu-toggler'),
+      menuTarget    = menuBtn.data('target'),
+      menuObj       = $('#' + menuTarget),
+      animateTime   = 300,
+      mobile_point  = 900;
+
+if ($('header').hasClass('size-lg')) {
+    header_point = 480;
+} else {
+    header_point = 280;
+}
+
+$('a[href^="#"]').click(function() {
+    var href        = $(this).attr('href'),
+        target      = $(href == "#" || href == "" ? 'html' : href),
+        position    = target.offset().top;
+
+    $('body,html').animate({scrollTop:position}, animateTime, 'swing');
+    return false;
+});
+
+scrollMenu($(window).scrollTop() > header_point)
+
+$(window).on('resize', function() {
+    window_size = $(window).width();
+    if (mobile_point < window_size) {
+        if (menuBtn.data('open') == false) {
+            openMenu(false);
+        }
+        menuObj.show();
+    } else {
+        if (menuBtn.data('open') == true) {
+            openMenu(true);
+        }
+        menuObj.hide();
+    }
+});
+
+$(window).on('scroll', function() {
+    if (window_size > mobile_point) {
+        scrollMenu($(this).scrollTop() > header_point)
+    }
+});
+
+menuObj.find('a').on('click', function() {
+    if (window_size <= mobile_point) {
+        openMenu(true);
+    } else {
+        openMenu(false);
+    }
+});
+
+$('#pageTop').on('click', function () {
+    scrollPosition(0);
+});
+
+$('.manu-toggler').on('click', function() {
+    openMenu($(this).data('open'));
+});
+
+function scrollPosition(position) {
+    $('body,html').animate({
+        scrollTop: position
+    });
+}
+
+function openMenu(is_open) {
+    if (!is_open) {
+        menuBtn.data('open', true);
+        menuObj.slideDown(animateTime);
+        menuBtn.addClass('open');
+    } else {
+        menuBtn.data('open', false);
+        menuObj.slideUp(animateTime);
+        menuBtn.removeClass('open');
+    }
+}
+
+function scrollMenu(is_scroll) {
+    if (is_scroll) {
+        menuObj.addClass('scroll');
+    } else {
+        menuObj.removeClass('scroll');
+    }
+}
